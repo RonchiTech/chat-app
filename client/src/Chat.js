bb2cc18bf791df2a94947deab0886f23922c837e
ronchitech <ronchibusiness@gmail.com>
2022-07-13 21:01:42 +0800

    other ways to display useQuery & useSubscription results

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery, useMutation, useSubscription } from '@apollo/client';
 import {
   messagesQuery,
   addMessageMutation,
   onMessageAddedSubscription,
 } from './graphql/queries';
-// import { addMessage, getMessages, onMessageAdded } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({ user }) => {
   const [messages, setMessages] = useState([]);
 
-  useQuery(messagesQuery, {
-    onCompleted: (result) => {
-      setMessages(result.messages);
-    },
+  const { data } = useQuery(messagesQuery, {
+    // onCompleted: (result) => {
+    //   setMessages(result.messages);
+    // },
   });
 
   useSubscription(onMessageAddedSubscription, {
     onSubscriptionData: (result) => {
-      setMessages(messages.concat(result.subscriptionData.data.onMessageAdded));
+      result.client.cache.writeQuery({
+        query: messagesQuery,
+        data: {
+          messages: messages.concat(
+            result.subscriptionData.data.onMessageAdded
+          ),
+        },
+      });
+      // setMessages(messages.concat(result.subscriptionData.data.onMessageAdded));
     },
   });
 
   const [addMessage] = useMutation(addMessageMutation);
+
+  useEffect(() => {
+    if (data) {
+      setMessages(data.messages);
+    }
+  }, [data]);
 
   const handleSend = async (text) => {
     await addMessage({ variables: { input: { text } } });
